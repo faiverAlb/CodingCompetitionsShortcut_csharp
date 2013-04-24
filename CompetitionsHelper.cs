16fb965ec84824f68746a8f75016ec44152ce84e
faiverAlb <faiver.alb@gmail.com>
2013-04-24 17:06:52 +0400

    Initial Commit
    Code competitional helper. Just copy Reader region

---
+++ CompetitionsHelper.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace CompetitionsHelperCoolNamespace
+{
+    public static class CompetitionsHelper
+    {
+        #region Reader
+
+        private static string Read()
+        {
+            return Console.ReadLine();
+        }
+
+        private static void Read(out int n1, out int n2)
+        {
+            string[] input = ReadArray();
+            n1 = int.Parse(input[0]);
+            n2 = int.Parse(input[1]);
+        }
+
+        private static void Read(out int n1, out int n2, out int n3)
+        {
+            string[] input = ReadArray();
+            n1 = int.Parse(input[0]);
+            n2 = int.Parse(input[1]);
+            n3 = int.Parse(input[2]);
+        }
+
+        private static void Read(out int n1, out int n2, out int n3, out int n4)
+        {
+            string[] input = ReadArray();
+            n1 = int.Parse(input[0]);
+            n2 = int.Parse(input[1]);
+            n3 = int.Parse(input[2]);
+            n4 = int.Parse(input[3]);
+        }
+
+        private static void Read<T>(out T a1, out T a2)
+        {
+            string[] input = ReadArray();
+            a1 = (T) Convert.ChangeType(input[0], typeof (T));
+            a2 = (T) Convert.ChangeType(input[1], typeof (T));
+        }
+
+        private static void Read<T>(out T a1, out T a2, out T a3)
+        {
+            string[] input = ReadArray();
+            a1 = (T) Convert.ChangeType(input[0], typeof (T));
+            a2 = (T) Convert.ChangeType(input[1], typeof (T));
+            a3 = (T) Convert.ChangeType(input[2], typeof (T));
+        }
+
+        private static void Read<T>(out T a1, out T a2, out T a3, out T a4)
+        {
+            string[] input = ReadArray();
+            a1 = (T) Convert.ChangeType(input[0], typeof (T));
+            a2 = (T) Convert.ChangeType(input[1], typeof (T));
+            a3 = (T) Convert.ChangeType(input[2], typeof (T));
+            a4 = (T) Convert.ChangeType(input[3], typeof (T));
+        }
+
+        private static int ReadInt()
+        {
+            return Int32.Parse(Read());
+        }
+
+        private static long ReadLong()
+        {
+            return long.Parse(Read());
+        }
+
+        private static double ReadDouble()
+        {
+            return double.Parse(Read(), CultureInfo.InvariantCulture);
+        }
+
+        private static T Read<T>()
+        {
+            return (T) Convert.ChangeType(Read(), typeof (T));
+        }
+
+        private static string[] ReadArray()
+        {
+            string readLine = Console.ReadLine();
+            if (readLine != null)
+                return readLine.Split(' ');
+
+            throw new ArgumentException();
+        }
+
+        private static List<int> ReadIntArray()
+        {
+            return ReadArray().Select(Int32.Parse).ToList();
+        }
+
+        private static List<long> ReadLongArray()
+        {
+            return ReadArray().Select(long.Parse).ToList();
+        }
+
+        private static List<double> ReadDoubleArray()
+        {
+            return ReadArray().Select(d => double.Parse(d, CultureInfo.InvariantCulture)).ToList();
+        }
+
+        private static List<T> ReadArray<T>()
+        {
+            return ReadArray().Select(x => (T) Convert.ChangeType(x, typeof (T))).ToList();
+        }
+
+        private static void WriteLine(int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        private static void WriteLine(double value)
+        {
+            Console.WriteLine(value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        private static void WriteLine(long value)
+        {
+            Console.WriteLine(value);
+        }
+
+        private static void WriteLine(double value, string stringFormat)
+        {
+            Console.WriteLine(value.ToString(stringFormat, CultureInfo.InvariantCulture));
+        }
+
+        private static void WriteLine<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+
+        private static void Write(int value)
+        {
+            Console.Write(value);
+        }
+
+        private static void Write(double value)
+        {
+            Console.Write(value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        private static void Write(long value)
+        {
+            Console.Write(value);
+        }
+
+        private static void Write(double value, string stringFormat)
+        {
+            Console.Write(value.ToString(stringFormat, CultureInfo.InvariantCulture));
+        }
+
+        private static void Write<T>(T value)
+        {
+            Console.Write(value);
+        }
+
+        #endregion
+    }
+}
